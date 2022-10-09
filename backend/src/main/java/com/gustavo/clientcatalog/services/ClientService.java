2bf5753d7e6f8e6c0adb9200232859e0cfe9fcaa
Gustavo Felipe Scheneider <gusfelipes@gmail.com>
2022-10-09 18:43:43 -0300

    feat: find all

---
+++ backend/src/main/java/com/gustavo/clientcatalog/services/ClientService.java
@@ -0,0 +1,24 @@
+package com.gustavo.clientcatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gustavo.clientcatalog.entities.Client;
+import com.gustavo.clientcatalog.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Client> findAll(){
+		return repository.findAll();
+	}
+	
+
+}

4f0dedeceee51ff774851e96db3479307c9dac9f
Gustavo Felipe Scheneider <gusfelipes@gmail.com>
2022-10-09 15:41:17 -0300

    first commit

---
+++ backend/src/test/java/com/gustavo/clientcatalog/ClientcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.clientcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
